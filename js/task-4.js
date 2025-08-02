8c26d9ee98395100610b0c38de48ad9fcaefd9e5
Reck1essness <crossdmitry@gmail.com>
2025-08-02 19:46:45 +0300

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const { email, password } = loginForm.elements;
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
 
+    if (emailValue === "" || passwordValue === "") {
+        allert("All form fields must be filled in");
+        return;
+    } 
+    
+    const formData = {
+        email: emailValue,
+        password: passwordValue,
+    };
+    console.log(formData);
+    loginForm.reset();
+});
